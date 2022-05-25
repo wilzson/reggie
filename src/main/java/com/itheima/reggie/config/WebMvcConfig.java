ecf74400a0f8f22cc3716337902fcd7fbe927c09
unknown <1041935459@qq.com>
2022-05-25 23:48:15 +0800

    后台登录模块

---
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.itheima.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * mvc的一个配置类，用于映射静态资源到浏览器中
+ */
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 设置我们的静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始静态资源映射");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
