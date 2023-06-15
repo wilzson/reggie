d1d15a36112f8330dbf83b4771c97522ff117a99
wilzson <1041935459@qq.com>
2023-06-15 10:24:50 +0800

    分类管理

--- src/main/java/com/itheima/reggie/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/EmployeeServiceImpl.java
@@ -1,8 +1,9 @@
-package com.itheima.reggie.service;
+package com.itheima.reggie.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.mapper.EmployeeMapper;
+import com.itheima.reggie.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 @Service
